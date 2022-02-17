7fe102f9fb555e6c7ec60e504c8c1517b99fe6ac
JanieYU <kg0101@adinsu.co.kr>
2022-02-17 16:25:24 +0900

    test v2

--- make-tree.py
+++ make-tree.py
@@ -1,17 +1,13 @@
-
-
-
-for a in range (5):
-    print("*"*(a+1))
-
 for b in range (4):
     print("*"*(2*b-1))
 
 for c in range(5):
     print(" "*(4-c)+"*"*(2*c-1))
 
-name = ("정성덕")
-sentence = input(str(name) +"은 바보래요")
+name = ("호랑이")
+sentence = input(str(name) +"는 바보가 아니래요")
 
 for d in range(5):
     print (" "*(d) + "*"*(2*(5-d)-1))
+
+print = ("고기 좋아요")
