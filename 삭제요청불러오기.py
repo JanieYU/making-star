75c04abf39d13a9bff41de70ac06c7578962d448
JanieYU <kg0101@adinsu.co.kr>
2022-02-17 15:59:40 +0900

    도전!

---
+++ 삭제요청불러오기.py
@@ -0,0 +1,18 @@
+
+""""rom mimetypes import init
+
+
+for a in range (5):
+    print("*"*(a+1))
+
+for b in range (4):
+    print("*"*(2*b-1))
+
+for c in range(5):
+    print(" "*(4-c)+"*"*(2*c-1))
+
+name = ("정성덕")
+sentence = input(str(name) +"은 바보래요")"""
+
+for d in range(5):
+    print (" "*(d) + "*"*(2*(5-d)-1))
